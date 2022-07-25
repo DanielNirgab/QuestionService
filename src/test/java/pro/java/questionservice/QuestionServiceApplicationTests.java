9f9f36d5ec58c023c3696c124a621f4dff1ce96d
DanielNigrab <bgrln347@gmail.com>
2022-07-25 22:10:52 +0300

    Initial commit

---
+++ src/test/java/pro/java/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.java.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
